13dc21eceeeedc4ae7a9bb456ff9576bdcc3a9d2
Liam Speakman <lspeakman001@gmail.com>
2024-06-01 18:39:11 -0400

    Replaced image link

--- src/commands/rules.ts
+++ src/commands/rules.ts
@@ -29,7 +29,7 @@ export async function execute(interaction: CommandInteraction) {
 	// Base embed for rules
 	let ruleEmbed = new EmbedBuilder()
 	.setColor(0x0099FF)
-	.setTitle('FINN SERVER RULES').setImage('https://media.discordapp.net/attachments/1040323190636937226/1161526051189571665/RulesPNG.png?ex=665b512e&is=6659ffae&hm=0e5e318849d078774d13f1af3319c562e0dbc0003cc2f74127396f7c7c309020&=&format=webp&quality=lossless&width=1440&height=288')
+	.setTitle('FINN SERVER RULES').setImage('https://evangoemer.com/images/finnbot/RulesPNG.webp')
 	.setFooter({ text: 'Rules created Nov 10 2022' });
 	// If no rule is given, print them all
 	if (ruleno == null) {
